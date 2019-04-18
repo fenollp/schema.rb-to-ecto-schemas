f08447e1e78ffb0a344f0ca9a6fa35e1c035ede6
Pierre Fenoll <pierrefenoll@gmail.com>
2019-04-18 16:45:01 +0200

    wip
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # schema.rb-to-ecto-schemas
 Ruby on Rails schema.rb to Elixir's Ecto schemas
+
+```
+./to_ecto.rb -i ~/my/db/schema.rb -o ~/my/ecto/models/ -n MyApp
+
+./to_ecto.rb -i ~/Documents/_panda/controle.git/db/schema.rb -o models -n EctoPanda --prefixes pubg,lol,csgo,dota2,ow --not-prefixes league,match,player,serie,team,tournament --inserted_at created_at
+```
---
+++ to_ecto.rb
@@ -0,0 +1,168 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+
+UPDATED_AT = 'updated_at'
+DEFAULT_INS = 'inserted_at'
+$args = {:PRX => [], :ins => DEFAULT_INS, :dt => 'utc_datetime'}
+OptionParser.new do |opt|
+  opt.on('-i', '--input FILE', 'Your schema.rb') { |o| $args[:file_in] = o }
+  opt.on('-o', '--output DIR', 'Where models will be written') { |o| $args[:dir_out] = o }
+  opt.on('-n', '--namespace NS', 'Your namespace') { |o| $args[:ns] = o }
+  opt.on('-p', '--prefixes PREFIXES', 'Comma separated table name prefixes to turn into subnamespaces') { |o| $args[:prx] = o.split(',') }
+  opt.on('-P', '--not-prefixes PREFIXES', 'Comma separated table name prefixes to not turn into subnamespaces') { |o| $args[:PRX] = o.split(',') }
+  opt.on('-c', '--inserted_at FIELD', 'Name of inserted_at timestamp field') { |o| $args[:ins] = o }
+  opt.on('-d', '--datetime TYPE', 'What to replace RoR `datetime` with') { |o| $args[:dt] = o }
+end.parse!
+
+abort 'Missing input file' unless $args[:file_in]
+abort 'Missing output directory' unless $args[:dir_out]
+abort 'Missing namespace' unless $args[:ns]
+
+$tables = {}
+
+module ActiveRecord
+  module Schema
+    def self.define(_opts, &block)
+      block.call()
+    end
+  end
+end
+
+def camelize(str)
+  str.split('_').map(&:capitalize).join()
+end
+
+def modulize(str, scope=nil)
+  s = str.dup
+  if str.end_with?('series')
+    # historical reasons
+    s = s.delete_suffix!('s')
+  elsif str.end_with?('ties')
+    # abilities => ability
+    s = s.delete_suffix!('ies') + 'y'
+  elsif str.end_with?('oes')
+    # heroes => hero
+    s = s.delete_suffix!('es')
+  elsif str.end_with?('ches')
+    # matches => match
+    s = s.delete_suffix!('es')
+  elsif str.end_with?('sses')
+    # processes => process
+    s = s.delete_suffix!('es')
+  elsif str.end_with?('s')
+    # teams => team
+    s = s.delete_suffix!('s')
+  end
+
+  pref = ''
+  if scope == nil
+    ss = s.split('_')
+    if $args[:prx].include?(ss[0])
+      pref = camelize(ss[0]) + '.'
+      s = s.delete_prefix!(ss[0])
+    end
+  else
+    ss = scope.split('_')
+    if !$args[:PRX].include?(s) && $args[:prx].include?(ss[0])
+      pref = camelize(ss[0]) + '.'
+    end
+  end
+  $args[:ns] + '.' + pref + camelize(s)
+end
+
+class Table
+  def initialize(name, opts=[])
+    @name = name
+    @opts = opts
+    @fields = {}
+    @belongs = {}
+  end
+
+  def dump
+    puts "defmodule #{modulize(@name)} do"
+    puts "  schema \"#{@name}\" do"
+    if @fields.key?($args[:ins]) && @fields.key?(UPDATED_AT)
+      # delete one of the two here so the other one can be replaced below
+      @fields.delete($args[:ins])
+    end
+    @fields.each do |field, details|
+      if field.end_with?('_id')
+        trimmed = field.dup.delete_suffix!('_id')
+        puts "    belongs_to(:#{trimmed}, #{modulize(trimmed,@name)})"
+      elsif field == UPDATED_AT
+        if $args[:ins] == DEFAULT_INS
+          puts '    timestamps()'
+        else
+          puts "    timestamps(inserted_at: :#{$args[:ins]})"
+        end
+      else
+        if details[:ty] == :datetime
+          puts "    field(:#{field}, :#{$args[:dt]})"
+        else
+          puts "    field(:#{field}, :#{details[:ty]})"
+        end
+      end
+    end
+    @belongs.each do |target, opts|
+      puts "    belongs_to(:#{target}, #{modulize(target,@name)})"
+    end
+    puts '  end'
+    puts 'end'
+  end
+
+  def index(field, opts=[])
+    # pass
+  end
+
+  [
+    :bigint,
+    :boolean,
+    :datetime,
+    :float,
+    :inet,
+    :integer,
+    :json,
+    :jsonb,
+    :string,
+    :text,
+    :uuid,
+  ].each do |ty|
+    define_method(:"#{ty}") do |field, opts=[]|
+      @fields[field] = {ty: __method__, opts: opts}
+      return
+    end
+  end
+
+  def belongs_to(target, opts)
+    @belongs[target] = opts
+  end
+end
+
+def enable_extension(name)
+  # pass
+end
+
+def create_table(name, opts, &block)
+  table = Table.new(name, opts)
+  block.call(table)
+  $tables[name] = table
+  return
+end
+
+def add_foreign_key(table, target, opts=[])
+  # TODO: has_one has_many
+  # https://haughtcodeworks.com/blog/software-development/ecto-schemas-on-rails/
+  puts "#{table}: #{nil == $tables[table]}"
+  ###### fuuuu https://stackoverflow.com/a/51479789/1418165
+  $tables[table].belongs_to(target, opts)
+  return
+end
+
+
+require $args[:file_in]
+
+$tables.each do |name, table|
+  puts table.dump()
+end
+puts $tables.length
